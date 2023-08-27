81bdf96c9e11733371602a9de056b68aab2ce518
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-27 20:58:04 +0200

    first commit

---
+++ controllers/jobs.js
@@ -0,0 +1,21 @@
+
+
+const getAllJobs = (req,res)=>{
+    res.send('Get All Jobs')
+}
+
+const getJob = (req,res)=>{
+    res.send('Get Job')
+}
+
+const createJob = (req,res)=>{
+    res.send('Create Job')
+}
+
+const updateJob = (req,res)=>{
+    res.send('Update Job')
+}
+
+const deleteJob = (req,res)=>{
+    res.send('Delete Job')
+}
