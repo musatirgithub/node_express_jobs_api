145e5800499829324d6f69cd57c06be43b048e53
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-29 22:26:22 +0200

    comparePasswords bugfix2

--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ UserSchema.methods.createJWT = function (){
   return jwt.sign({userId:this._id, name:this.name}, process.env.JWT_SECRET, {expiresIn:process.env.JWT_LIFETIME})
 }
 
-UserSchema.methods.comparePasswords = async function (candidatePassword){
+UserSchema.methods.comparePassword = async function (candidatePassword){
   const isMatch = await bcrypt.compare(candidatePassword, this.password)
   return isMatch
 }
