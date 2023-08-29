6d38a951d410823939a003e67880fe55511db9ff
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-29 22:16:37 +0200

    passwords compared

--- models/User.js
+++ models/User.js
@@ -35,4 +35,9 @@ UserSchema.methods.createJWT = function (){
   return jwt.sign({userId:this._id, name:this.name}, process.env.JWT_SECRET, {expiresIn:process.env.JWT_LIFETIME})
 }
 
+UserSchema.methods.comparePasswords = async function (candidatePassword){
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
+
 module.exports = mongoose.model('User', UserSchema)
