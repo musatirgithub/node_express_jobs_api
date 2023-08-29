dc70cc22433ae18fccd60c6b4657b1042dc62919
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-29 23:44:55 +0200

    bug fix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,7 @@ const {UnauthenticatedError} = require('../errors')
 const auth = async (req,res,next)=>{
     //check header
     const authHeader = req.headers.authorization
-    if (!authHeader || !authHeader.startswith('Bearer ')){
+    if (!authHeader || !authHeader.startsWith('Bearer ')){
         throw new UnauthenticatedError('Authentication Invalid')
     }
 
