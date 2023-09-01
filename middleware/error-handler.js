f04536dd0313183b28df6889d2ae81b69b9b6951
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-01 23:42:32 +0200

    error-handler minor CustomAPI instance checkCancel

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,9 +7,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg:err.message || 'Something went wrong. Tray again later.'
   }
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} please choose another value`;
     customError.statusCode = 400;
