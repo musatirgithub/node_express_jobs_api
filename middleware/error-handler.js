83e78c1c6872d535f4e505f140013f5fb8b9086a
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-02 00:11:37 +0200

    Cast & Validation errors added to the errorhandler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,6 +10,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
+
+  if(err.name && err.name==='ValidationError'){
+    customError.msg = Object.values(err.errors).map((item)=>item.message).join(', ');
+    customError.statusCode = 400;
+  }
+
+  if(err.name && err.name==='CastError'){
+    customError.msg = `No item found with the id: ${err.value}`;
+    customError.statusCode = 400;
+  }
+
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} please choose another value`;
     customError.statusCode = 400;
