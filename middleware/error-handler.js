8364ccb8fa9b5b84dce5181c9b12c6b4e044aa20
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-01 22:34:58 +0200

    customError object is used in mongoose error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,16 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
+
+  const customError = {
+    // set default
+    statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg:err.msg || 'Something went wrong. Tray again later.'
+  }
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg:customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
