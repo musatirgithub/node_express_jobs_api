99310da46c05a7ef81536af8a5508a773abc10bb
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-02 23:43:00 +0200

    security middleware added to app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('express-async-errors');
 const helmet = require('helmet');
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimit = require('express-rate-limit')
+const rateLimiter = require('express-rate-limit')
 
 
 const express = require('express');
@@ -22,6 +22,18 @@ const jobsRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: RateLimit-* headers; draft-7: combined RateLimit header
+	legacyHeaders: false, // X-RateLimit-* headers
+	// store: ... , // Use an external store for more precise rate limiting
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.use(express.json());
 // extra packages
